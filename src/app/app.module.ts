2cf08c8d868c8ae2b17d280354654dc2b65ed5bc
baohan <65202910+baohodn93@users.noreply.github.com>
2022-04-02 22:07:44 +0900

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { HttpClientModule } from '@angular/common/http';
 import {httpInterceptorProviders} from './services/auth.interceptor';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,13 +21,21 @@ import { RegisterComponent } from './form-login/register/register.component';
 import { FontSizeComponent } from './shared/font-size/font-size.component';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './form-login/login/login.component';
-import { UserInforComponent } from './form-login/user-infor/user-infor.component';
+import { UserInfoComponent } from './form-login/user-info/user-info.component';
+import { ChangePasswordComponent } from './form-login/manager/change-password/change-password.component';
+import { ChangeAvatarComponent } from './form-login/manager/change-avatar/change-avatar.component';
+import { ChangeRoleComponent } from './form-login/manager/change-role/change-role.component';
+import { ResetPasswordComponent } from './form-login/manager/reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, data: { title: 'Home' } },
   { path: 'login', component: LoginComponent, data: {title: 'Login'}},
   { path: 'register', component: RegisterComponent, data: {title: 'Register'}},
-  { path: 'change-profile', component: UserInforComponent, data: {title: 'Change-Profile'}},
+  { path: 'change-profile', component: UserInfoComponent, data: {title: 'Change-Profile'}},
+  { path: 'change-avatar', component: ChangeAvatarComponent, data: {title: 'Change-Avatar'}},
+  { path: 'change-password', component: ChangePasswordComponent, data: {title: 'Change-Password'}},
+  { path: 'change-role', component: ChangeRoleComponent, data: {title: 'Change-Role'}},
+  { path: 'password-reset', component: ResetPasswordComponent, data: {title: 'Password-Reset'}},
 ];
 
 @NgModule({
@@ -38,11 +47,16 @@ const routes: Routes = [
     RegisterComponent,
     FontSizeComponent,
     LoginComponent,
-    UserInforComponent
+    UserInfoComponent,
+    ChangePasswordComponent,
+    ChangeAvatarComponent,
+    ChangeRoleComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
@@ -51,7 +65,6 @@ const routes: Routes = [
     MatIconModule,
     MatCardModule,
     MatButtonModule,
-    FormsModule,
     ReactiveFormsModule,
     RouterModule.forRoot(routes)
   ],
